d56732f44aea39ddcf920abffab1f5dd358feea1
isaacl81 <isaaclin2007@gmail.com>
2023-10-13 09:11:22 -0400

    notes

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("connected")
 
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".first-name"),
+}
+
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.firstName.value);
+});
