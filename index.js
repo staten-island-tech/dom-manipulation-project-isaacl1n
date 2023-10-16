cc2c7648d9ee68091fbcb7c03bcaea218b1a460b
isaacl81 <isaaclin2007@gmail.com>
2023-10-16 09:25:36 -0400

    text area

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const DOMSelectors = {
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
+    console.log(DOMSelectors.form.value);
 });
