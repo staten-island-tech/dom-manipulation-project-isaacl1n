85850af5cfe22d6d2d0b4f6be82da926aa32c085
isaacl81 <isaaclin2007@gmail.com>
2023-10-17 21:37:55 -0400

    final

--- index.js
+++ index.js
@@ -18,20 +18,23 @@ DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     const taskName = DOMSelectors.taskName.value;
     const taskDesc = DOMSelectors.taskDescription.value;
-    const dueDate = DOMSelectors.dueDate.value;
+
+    const dueDateValue = DOMSelectors.dueDate.value;
+    const dueDate = new Date(dueDateValue);
+    const formattedDueDate = dueDate.toLocaleDateString() + ", " + dueDate.toLocaleTimeString();
 
     const taskCard = document.createElement("div");
     taskCard.classList.add("task-card");
     taskCard.innerHTML = `
         <h2 class="task-name">${taskName}</h2>
         <p class="task-desc">${taskDesc}</p>
-        <h4 class="due-date">Due: ${dueDate}</h4>
+        <h4 class="due-date">Due: ${formattedDueDate}</h4>
         <button type="button" class="remove-button">I'm finished with this task.</button>
     `;
     DOMSelectors.taskList.appendChild(taskCard);
 
     clear();
-    
+
     const removeButton = taskCard.querySelector(".remove-button");
     removeButton.addEventListener("click", function() {
         taskCard.remove();
