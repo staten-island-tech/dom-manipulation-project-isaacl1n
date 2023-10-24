6a492ea4db8de0bf80bf878864c4b5205b835d46
isaacl81 <isaaclin2007@gmail.com>
2023-10-24 09:25:18 -0400

    redo InnerHTML

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("connected")
 const DOMSelectors = {
     form: document.querySelector("#form"),
     taskList: document.querySelector("#tasks"),
+    taskCard: document.querySelector(".task-card"),
     taskName: document.getElementById("task-name"),
     taskDescription: document.getElementById("task-desc"),
     dueDate: document.getElementById("due-date"),
@@ -23,15 +24,17 @@ DOMSelectors.form.addEventListener("submit", function(event){
     const dueDate = new Date(dueDateValue);
     const formattedDueDate = dueDate.toLocaleDateString() + ", " + dueDate.toLocaleTimeString();
 
-    const taskCard = document.createElement("div");
-    taskCard.classList.add("task-card");
-    taskCard.innerHTML = `
-        <h2 class="task-name">${taskName}</h2>
-        <p class="task-desc">${taskDesc}</p>
-        <h4 class="due-date">Due: ${formattedDueDate}</h4>
-        <button type="button" class="remove-button">I'm finished with this task.</button>
-    `;
-    DOMSelectors.taskList.appendChild(taskCard);
+    DOMSelectors.
+
+    // const taskCard = document.createElement("div");
+    // taskCard.classList.add("task-card");
+    // taskCard.innerHTML = `
+    //     <h2 class="task-name">${taskName}</h2>
+    //     <p class="task-desc">${taskDesc}</p>
+    //     <h4 class="due-date">Due: ${formattedDueDate}</h4>
+    //     <button type="button" class="remove-button">I'm finished with this task.</button>
+    // `;
+    // DOMSelectors.taskList.appendChild(taskCard);
 
     clear();
 
